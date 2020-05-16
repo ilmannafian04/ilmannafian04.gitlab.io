a918b7d103b906eb6b6c0ad1f7db66fc0dcb6f78
Ilman Nafian <milmannafian04@gmail.com>
2020-05-16 13:55:08 +0700

    new skill design

--- src/app/Skill.tsx
+++ src/app/Skill.tsx
@@ -1,69 +1,65 @@
-import { Typography } from '@material-ui/core';
+import { createStyles, Typography } from '@material-ui/core';
 import Box from '@material-ui/core/Box';
 import React, { FunctionComponent } from 'react';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
-import Divider from '@material-ui/core/Divider';
+import makeStyles from '@material-ui/core/styles/makeStyles';
+
+const useStyles = makeStyles(() =>
+    createStyles({
+        skillItem: {
+            padding: 1,
+        },
+        skillSection: {
+            minWidth: '15rem',
+        },
+    }),
+);
 
 interface SkillSectionProps {
-    proficiency: string;
+    categories: string;
     skills: string[][];
 }
 
 const skillsStub: SkillSectionProps[] = [
     {
-        proficiency: 'Good',
-        skills: [
-            ['Django', 'NestJS'],
-            ['Python', 'JavaScript', 'TypeScript'],
-            ['Docker', 'GitLab CI'],
-        ],
+        categories: 'Framework',
+        skills: [['Django'], ['ReactJS'], ['NestJS'], ['Angular', 'Spring']],
     },
     {
-        proficiency: 'Good',
-        skills: [
-            ['Django', 'NestJS'],
-            ['Python', 'JavaScript', 'TypeScript'],
-            ['Docker', 'GitLab CI'],
-        ],
+        categories: 'Technology',
+        skills: [['GIT', 'GitLab CI'], ['Docker'], ['GraphQL', 'MQ'], ['gRPC', 'ELK stack']],
     },
     {
-        proficiency: 'Good',
-        skills: [
-            ['Django', 'NestJS'],
-            ['Python', 'JavaScript', 'TypeScript'],
-            ['Docker', 'GitLab CI'],
-        ],
-    },
-    {
-        proficiency: 'Good',
-        skills: [
-            ['Django', 'NestJS'],
-            ['Python', 'JavaScript', 'TypeScript'],
-            ['Docker', 'GitLab CI'],
-        ],
+        categories: 'Language',
+        skills: [['Python', 'Javascript', 'Typescript'], ['Java', 'HTML', 'SQL'], ['CSS'], ['C']],
     },
 ];
 
 const SkillSection: FunctionComponent<SkillSectionProps> = (props: SkillSectionProps) => {
+    const classes = useStyles();
     return (
-        <Box flexDirection="column">
-            <Typography variant="h4">{props.proficiency}</Typography>
+        <Box flexDirection="column" flexGrow={1} ml={1} my={2}>
+            <Box pr={5}>
+                <Typography variant="h4">{props.categories}</Typography>
+            </Box>
             <List>
                 {props.skills.map((skillList, index) => {
-                    return (
-                        <Box key={index}>
-                            {skillList.map((skill, index1) => {
-                                return (
-                                    <ListItem key={`${index}-${index1}`}>
-                                        <ListItemText primary={skill} />
-                                    </ListItem>
-                                );
-                            })}
-                            {index < props.skills.length - 1 ? <Divider /> : null}
-                        </Box>
-                    );
+                    return skillList.map((skill, index1) => {
+                        return (
+                            <ListItem key={`${index}-${index1}`} className={classes.skillItem}>
+                                <ListItemText
+                                    disableTypography
+                                    primary={
+                                        <Typography color={index < 2 ? 'textPrimary' : 'textSecondary'}>
+                                            {skill}
+                                        </Typography>
+                                    }
+                                />
+                            </ListItem>
+                        );
+                    });
                 })}
             </List>
         </Box>
@@ -74,9 +70,9 @@ const Skill: FunctionComponent = () => {
     return (
         <>
             <Typography variant="h2">Skills</Typography>
-            <Box display="flex" flexWrap="wrap" justifyContent="space-between" mt={1}>
+            <Box display="flex" flexWrap="wrap" justifyContent="space-around" mt={1}>
                 {skillsStub.map((value, index) => {
-                    return <SkillSection key={index} proficiency={value.proficiency} skills={value.skills} />;
+                    return <SkillSection key={index} categories={value.categories} skills={value.skills} />;
                 })}
             </Box>
         </>
