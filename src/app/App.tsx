b9132b77c5fcf7c540c3ec726b55c8a3ad390760
Ilman Nafian <milmannafian04@gmail.com>
2020-05-16 13:56:55 +0700

    add bottom padding to the entire page

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,15 +4,18 @@ import Container from '@material-ui/core/Container';
 import Intro from './Intro';
 import Navbar from './Navbar';
 import Skill from './Skill';
+import { Box } from '@material-ui/core';
 
 const App: FunctionComponent = () => {
     return (
         <>
             <Navbar />
-            <Container>
-                <Intro />
-                <Skill />
-            </Container>
+            <Box pb={2}>
+                <Container>
+                    <Intro />
+                    <Skill />
+                </Container>
+            </Box>
         </>
     );
 };
