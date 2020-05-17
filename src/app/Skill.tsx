ad2840613a658d6f14b3727ecfccf6a0490c4473
Ilman Nafian <milmannafian04@gmail.com>
2020-05-17 19:41:32 +0700

    add skeleton as a placeholder while loading or when an error happened

--- src/app/Skill.tsx
+++ src/app/Skill.tsx
@@ -5,6 +5,7 @@ import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
 import makeStyles from '@material-ui/core/styles/makeStyles';
+import Skeleton from '@material-ui/lab/Skeleton';
 import { gql } from 'apollo-boost';
 import React, { FunctionComponent } from 'react';
 
@@ -66,7 +67,7 @@ const SkillSection: FunctionComponent<SkillSectionProps> = (props: SkillSectionP
                 <>
                     {skills.map((skillInProficiency, index1) =>
                         skillInProficiency.map((skill, index2) => (
-                            <ListItem key={`${index1}-${index2}`} className={classes.skillItem}>
+                            <ListItem key={`section-item-${index1}-${index2}`} className={classes.skillItem}>
                                 <ListItemText
                                     disableTypography
                                     primary={
@@ -82,7 +83,15 @@ const SkillSection: FunctionComponent<SkillSectionProps> = (props: SkillSectionP
             );
         }
     }
-    return <Typography>Loading/Error</Typography>;
+    const skeletons = [];
+    for (let i = 0; i < Math.floor(Math.random() * (7 - 3 + 1)) + 3; i++) {
+        skeletons.push(
+            <ListItem key={`section-skeleton-${props.category}-${i}`}>
+                <ListItemText primary={<Skeleton width={`${Math.floor(Math.random() * (60 - 20 + 1)) + 20}%`} />} />
+            </ListItem>,
+        );
+    }
+    return <>{skeletons.map((skeleton) => skeleton)}</>;
 };
 
 const Skill: FunctionComponent = () => {
@@ -96,9 +105,11 @@ const Skill: FunctionComponent = () => {
             <Box display="flex" flexWrap="wrap" justifyContent="space-around" mt={1}>
                 {['framework', 'technology', 'language'].map((category, index) => {
                     return (
-                        <Box key={index} flexDirection="column" flexGrow={1} ml={1} my={2}>
+                        <Box key={`section-${category}-${index}`} flexDirection="column" flexGrow={1} ml={1} my={2}>
                             <Box pr={5}>
-                                <Typography variant="h4">{category.toUpperCase()}</Typography>
+                                <Typography variant="h4">
+                                    {category.charAt(0).toUpperCase() + category.slice(1)}
+                                </Typography>
                             </Box>
                             <List>
                                 <SkillSection category={category} loading={loading} skillsData={data} />
