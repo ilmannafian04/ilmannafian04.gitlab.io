485828c8542b987b42f50a12312e2e98bc5b896d
Ilman Nafian <milmannafian04@gmail.com>
2020-05-18 17:04:45 +0700

    skill form

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import { ApolloProvider } from '@apollo/react-hooks';
 import { CssBaseline } from '@material-ui/core';
 import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
 import ApolloClient from 'apollo-boost';
+import Axios from 'axios';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { HashRouter } from 'react-router-dom';
@@ -11,6 +12,8 @@ import App from './app/App';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
 
+Axios.defaults.baseURL = 'https://ilmannafian04.herokuapp.com/';
+
 const theme = createMuiTheme({
     palette: {
         type: 'dark',
@@ -19,6 +22,14 @@ const theme = createMuiTheme({
 
 const client = new ApolloClient({
     uri: 'https://ilmannafian04.herokuapp.com/graphql',
+    request: (operation) => {
+        const token = window.sessionStorage.getItem('yep')
+        operation.setContext({
+            headers: {
+                authorization: token ? `Bearer ${token}` : ''
+            }
+        })
+    }
 });
 
 ReactDOM.render(
